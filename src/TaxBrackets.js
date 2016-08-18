d0b9e0323c2f88535a9a4e26af34cfa0d524beed
Heath Jones <hj1980@gmail.com>
2016-08-18 22:44:05 +1000

    Add FY2013 Tax rate information

---
+++ src/TaxBrackets.js
@@ -0,0 +1,20 @@
+const TaxBrackets = {
+  FY2013: [{
+		rate: 0,
+    upperLimit: 18200
+  }, {
+		rate: 0.19,
+    upperLimit: 37000
+  }, {
+		rate: 0.325,
+    upperLimit: 80000
+  }, {
+		rate: 0.37,
+    upperLimit: 180000
+  }, {
+		rate: 0.45,
+    upperLimit: null
+  }]
+};
+
+module.exports = TaxBrackets;
